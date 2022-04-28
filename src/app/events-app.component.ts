8fa15272be7c157c82427f3dd6e19bde2b65cc6f
Jesus de la Fuente <jfuentelucas@gmail.com>
2022-04-28 22:37:47 +0200

    first commit

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: `
+    <h1>Events-App</h1>
+    <img src="assets/images/basic-shield.png">
+    
+    `
+})
+export class EventsAppComponent {
+  title = 'Events-App';
+}
