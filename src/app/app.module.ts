14d55d6b6624af98a966905258c9bf7a8b24a996
Jesus de la Fuente <jfuentelucas@gmail.com>
2022-04-29 09:55:42 +0200

    communicating with child component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component'
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+
 
 @NgModule({
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent
+    
+
   ],
   imports: [
     BrowserModule
---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'event-list',
+    template:`
+    <div>
+        <h1>Upcoming Events:</h1>  
+    <hr/>
+    <event-thumbnail [event]="event1"></event-thumbnail>
+
+</div>
+    `
+})
+
+export class EventsListComponent {
+    event1 = {
+        id:1,
+        name:'Angular Connect',
+        date:'8/24/2025',
+        time:'08:00 am',
+        price:1000,
+        imageUrl:'../../assets/images/angularconnect-shield.png',
+        location:{
+            address:'666 DT',
+            city:'Liverpool',
+            country:'England'
+        }
+    }
+}
