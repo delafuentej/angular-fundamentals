d38abce2aafc1533e54dd60a50f0386797ed8b3e
Jesus de la Fuente <jfuentelucas@gmail.com>
2022-05-01 12:02:58 +0200

    using models for type safety

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
+import { IEvent } from './shared/index';
 
 @Component({
     selector:'event-thumbnail',
@@ -39,7 +39,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 
 export class EventThumbnailComponent{
-   @Input() event:any;
+   @Input() event:IEvent;
 
    getStartTimeClass(){
        const isEarlyStart = this.event && this.event.time ==='8:00 am';
