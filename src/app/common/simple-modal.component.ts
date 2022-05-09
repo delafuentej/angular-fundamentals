d5a9db04ce9af24202299f8ea9b6eed93bf5f2fa
Jesus de la Fuente <jfuentelucas@gmail.com>
2022-05-09 15:00:40 +0200

    creating settings on components

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -1,12 +1,15 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, ViewChild, ElementRef, Inject } from '@angular/core';
+import { JQ_TOKEN } from './jQuery.service';
 
 @Component({
     selector:'simple-modal',
     template: `
         <div 
+
             id="{{elementId}}"
             class="modal fade"
             tabindex="-1"
+            #modalContainer
             >
 
             <div class="modal-dialog">
@@ -26,10 +29,10 @@ import { Component, Input } from "@angular/core";
 
                     </div> 
                     <div
-                        class="modal.body">
-                            <ng-content>
-
-                            </ng-content>
+                        class="modal-body"
+                        (click)="closeModal()"
+                        >
+                            <ng-content></ng-content>
                     </div>     
 
                 </div>              
@@ -48,4 +51,13 @@ import { Component, Input } from "@angular/core";
 export class SimpleModalComponent{
     @Input() title: string;
     @Input() elementId: string;
+    @Input() closeOnBodyClick: string;
+    @ViewChild('modalContainer') containerEl: ElementRef;
+
+    constructor(@Inject(JQ_TOKEN) private $:any){}
+
+    closeModal(){
+        if(this.closeOnBodyClick.toLocaleLowerCase() ==='true')
+        this.$(this.containerEl.nativeElement).modal('hide');
+    }
 } 
